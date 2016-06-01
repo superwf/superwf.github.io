2de09505b7abde1a20bd9d2a17de5462338a7405
superwf <superwf@gmail.com>
2016-06-01 16:30:32 +0800

    use webpack es6 and d3.js for rebuild all

---
+++ app/components/circle.vue
@@ -0,0 +1,15 @@
+<template>
+  <circle cx='40', cy='40', r='{{circle.r}}', fill='{{fill}}'></circle>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        circle: {
+          r: 10,
+        },
+        fill: 'red'
+      }
+    }
+  }
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+var webpack = require('webpack')
+
+module.exports = {
+  entry: {
+    index: './app/js/index.js',
+  },
+  output: {
+    // path: __dirname + '/public/js',
+    path: './',
+    filename: '[name].js',
+    publicPath: '/',
+  },
+
+  // 定位到行与列
+  // devtool: 'source-map',
+  // 定位到行,但是编译会快，适合开发用
+  devtool: 'eval-source-map',
+
+  module: {
+    preLoaders: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'eslint',
+    }],
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+      },
+      {test: /\.vue$/, loader: 'vue'},
+      {test: /\.json$/, loader: 'json'},
+      {test: /\.html$/, loader: 'html'},
+      {test: /\.css$/, loader: 'style!css'},
+      {test: /\.less$/, loader: 'style!css!less'},
+      {
+        test: /\.jade$/,
+        loader: 'jade-loader',
+      },
+      {test: /\.(eot|woff|ttf|svg|woff2)$/, loader: 'url-loader'},
+      {test: /\.png$/, loader: 'url-loader?limit=20192',
+        query: {mimetype: 'image/png'}
+      },
+    ]
+  },
+  // postcss: [autoprefixer({browsers: ['last 2 versions']})],
+
+  plugins: [
+    new webpack.WatchIgnorePlugin([/node_modules/]),
+  ],
+}
